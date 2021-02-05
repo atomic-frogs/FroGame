be34016c86bc586c55d1425c53ae0bc91a978c2c
Nathan Thomas Valentim Sircus <36446586+xinove1@users.noreply.github.com>
2021-02-05 12:09:03 -0300

    Varias coisas
    Walljump, incompleto
    interruptor, on off
    blackground com animação

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,7 @@ var dir = 0
 onready var raycast = $RayCast2D
 onready var sprite = $player_sprite
 onready var animationPlayer = $AnimationPlayer
+onready var blackground = $Blackground
 
 func get_input():
 	dir = 0
@@ -24,8 +25,14 @@ func get_input():
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
-		
 		velocity.x = lerp(velocity.x, 0, friction)
+	
+	if !$walljumptimer.is_stopped():
+		dir = 0
+
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		return true
 
 func _physics_process(delta):
 	get_input()
@@ -33,7 +40,7 @@ func _physics_process(delta):
 		if dir == 0 :
 #			print("stand")
 			animationPlayer.play("Stand")
-		if Input.is_action_just_pressed("jump"):
+		if jump():
 			velocity.y = jump_speed
 			animationPlayer.play("Jump")
 			print("jump")
@@ -42,6 +49,27 @@ func _physics_process(delta):
 			print("caindo")
 #			animationPlayer.animation_set_next("Jump","Fall")
 			animationPlayer.play("Fall")
+		if is_on_wall():
+			if dir != 0:
+				velocity.y = 0
+				
+				if jump():
+					
+					$walljumptimer.start()
+					velocity.y = jump_speed
+					velocity.x = (dir * -1) * 150
+					
+					animationPlayer.play("Jump")
+					
+					
+	
+	
+	if Global.off:
+		blackground.show()
+		blackground.play()
+	else:
+		blackground.hide()
+		blackground.stop()
 
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
